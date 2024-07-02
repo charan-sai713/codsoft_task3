1bdfc65ffba88e2f0f02d87dd2bac66e36895aa6
charan-sai713 <ponnaboinacharansai@gmail.com>
2024-07-03 00:46:38 +0530

    Add files via upload

---
+++ password generator.py
@@ -0,0 +1,12 @@
+import secrets
+import string
+import random
+letters = string.ascii_letters
+digits = string.digits
+special_chars = string.punctuation
+selection_list = letters + digits + special_chars
+password_len = int(input("enter the length of your password :"))
+password = ''
+for i in range(password_len):
+ password+= ''.join(secrets.choice(selection_list))
+print(password)
